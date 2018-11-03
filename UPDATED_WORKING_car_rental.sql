c6a343bb0ddab42a77845364087d97843f03872a
nicoarl <nico.adelmann@hotmail.com>
2018-11-03 17:07:55 +0100

    working now

--- UPDATED_WORKING_car_rental.sql
+++ UPDATED_WORKING_car_rental.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 03, 2018 at 04:55 PM
+-- Generation Time: Nov 03, 2018 at 05:07 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
